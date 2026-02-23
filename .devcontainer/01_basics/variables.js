10a2b34317449bd08847eb5764a638ee88edcbc8
2k22csai32026-png <2k22.csai.32026@gmail.com>
2026-02-23 20:00:52 +0000

    container config done

---
+++ .devcontainer/01_basics/variables.js
@@ -0,0 +1,5 @@
+const accountId=12345//cannot change once declared
+let acountEmail="gaurav@gmail.com"
+var accountPass="1234567890"
+accountCity="Kanpur"
+console.table([accountId,accountPass,acountEmail,accountCity])
