38b7b3f3ff460f8f1431e3f9310a4e4eb03bf8ff
Gaurav <2k22.csai.32026@gmail.com>
2026-02-24 17:04:43 +0530

    conversion

--- .devcontainer/01_basics/conversion.js
+++ .devcontainer/01_basics/conversion.js
@@ -1,11 +1,11 @@
 let score = "22abc"
-console.log(typeof score);
+// console.log(typeof score);
 
 let inNumber=Number(score)
 
-console.log(typeof inNumber);
+// console.log(typeof inNumber);
 
-console.log(inNumber);//NaN (not a number)
+// console.log(inNumber);//NaN (not a number)
 
 //"33"=>33
 // "33abs"=>NaN
@@ -18,7 +18,7 @@ let isLoggedIn="Gaurav"
 // let isLoggedIn =1
 let booleanIsLogedIn = Boolean(isLoggedIn)
 
-console.log(booleanIsLogedIn);
+// console.log(booleanIsLogedIn);
 
 //"String"=>true
 // "" => false
@@ -27,7 +27,25 @@ console.log(booleanIsLogedIn);
 let number=28
 
 let toString=String(number)
-console.log(toString);
-console.log(typeof toString);
+// console.log(toString);
+// console.log(typeof toString);
+//  ***************************operations*******************
 
+let value=3
+let negVal=-value
+// console.log(negVal);
+
+
+let str1="Gaurav"
+let str2=" Jaiswal"
+
+let str=str1+str2
+
+console.log(str);
+
+console.log("1"+2);
+
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");//give the priority to the first term
 
