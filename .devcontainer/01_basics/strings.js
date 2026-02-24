d7e0e4e1d6675fca6c039704af142964e180efb6
Gaurav <2k22.csai.32026@gmail.com>
2026-02-24 23:17:23 +0530

    changes

--- .devcontainer/01_basics/strings.js
+++ .devcontainer/01_basics/strings.js
@@ -1,4 +1,16 @@
 const name="Gaurav";
 const repoCount=50
 
-console.log(name+repoCount);
+// console.log(name+repoCount+"Jaiswal");
+
+console.log(`Hello my name is ${name} and my repocount is ${repoCount}`);
+
+const gameName=new String('GTA');
+
+console.log(gameName);
+// in this 0:G, 1:T, 3:A length=>3 in key value pairs
+console.log(gameName[1]);//T
+console.log(gameName.__proto__);//{}
+
+console.log(gameName.length);
+
