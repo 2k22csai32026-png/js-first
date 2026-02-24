d73ec2777ff4aac3f29e79698159ae7e57d574c6
Gaurav <2k22.csai.32026@gmail.com>
2026-02-24 17:39:22 +0530

    changed

---
+++ .devcontainer/01_basics/nonpremitive.js
@@ -0,0 +1,22 @@
+arr=["Gaurav",1,"salman"]
+
+// object
+let obj= {
+    name:"Gaurav",
+    age:24,
+    batch:2026
+}
+
+console.log(obj);
+
+
+// function
+
+ function hello(){
+    console.log("Hello world !");
+    
+}
+hello();
+
+// return tyoe of all the non premitive datatype will be 'object'
+
