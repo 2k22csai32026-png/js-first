43465dc5a0671660c9abde9d649a13d1ef0269f3
Gaurav <2k22.csai.32026@gmail.com>
2026-02-24 16:26:07 +0530

    conversion

---
+++ .devcontainer/01_basics/conversion.js
@@ -0,0 +1,33 @@
+let score = "22abc"
+console.log(typeof score);
+
+let inNumber=Number(score)
+
+console.log(typeof inNumber);
+
+console.log(inNumber);//NaN (not a number)
+
+//"33"=>33
+// "33abs"=>NaN
+// null=>0
+// undefined=>NaN
+// true=>1 , false=>0
+
+
+let isLoggedIn="Gaurav"
+// let isLoggedIn =1
+let booleanIsLogedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLogedIn);
+
+//"String"=>true
+// "" => false
+// 1=> true, 0=>false
+
+let number=28
+
+let toString=String(number)
+console.log(toString);
+console.log(typeof toString);
+
+
---
+++ datatypes.js
@@ -0,0 +1,2 @@
+let name="Gaurav"
+let age=24
