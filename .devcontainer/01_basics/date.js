b339e040efe03a947ba27d528486784c6fa2e422
Gaurav <2k22.csai.32026@gmail.com>
2026-02-28 01:35:23 +0530

    change

---
+++ .devcontainer/01_basics/date.js
@@ -0,0 +1,16 @@
+let myDate = new Date()
+// console.log(myDate);
+
+// console.log(myDate.toString());
+
+// console.log(myDate.toLocaleString());
+
+
+let myCreatedDate = new Date(2002,8,18)
+
+console.log(myCreatedDate.toDateString());
+
+
+let myTimeStamp = Date.now()
+
+console.log(myTimeStamp);// milli second
