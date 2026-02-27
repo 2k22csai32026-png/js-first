e2f2d7c5998f37abeb737b14fef1c8407d97cc04
Gaurav <2k22.csai.32026@gmail.com>
2026-02-27 18:31:42 +0530

    changes

---
+++ .devcontainer/01_basics/math.js
@@ -0,0 +1,19 @@
+const score = 400;
+
+console.log(score);
+ 
+const balance = new Number(100)//object
+
+console.log(balance);
+
+console.log(balance.toFixed(3));//gives values after decimal
+
+const hundreds=100000
+
+console.log(hundreds.toLocaleString());//give commas in the number according to the USA standards
+
+console.log(hundreds.toLocaleString('en-IN'));
+
+console.log(Math.random());//gives random values between 0 and 1
+
+
