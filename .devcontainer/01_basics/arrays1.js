a81a6388c3bd90bc36c4ceac35c5ff2124a10e23
Gaurav <2k22.csai.32026@gmail.com>
2026-02-28 16:46:47 +0530

    change

---
+++ .devcontainer/01_basics/arrays1.js
@@ -0,0 +1,28 @@
+const arr=[1,3,5,7]
+
+console.log(arr.includes(1));//check if the element exists in the array
+
+console.log(arr.push(9));
+
+console.log(arr);
+
+console.log(arr.length);
+
+const newArr = arr.join()
+
+console.log(typeof newArr);
+
+console.log("A");
+
+const ar1=[1,2,3,4,5,6,7,8,9]
+
+console.log(ar1.slice(1,3));
+
+console.log(ar1);
+
+console.log(ar1.splice(1,4));//it will make change in ariginal array also
+
+
+console.log(ar1);
+
+
