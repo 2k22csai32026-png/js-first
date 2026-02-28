d38fe3861b53f3ba4f6e198267362bb8c9dfd63d
Gaurav <2k22.csai.32026@gmail.com>
2026-03-01 02:15:17 +0530

    change

---
+++ .devcontainer/controlflow/nullish.js
@@ -0,0 +1,14 @@
+// Nullish Coalescing Operator(??): null undefined
+
+let val1;
+val1 = 5 ?? 10
+val1 = null ?? 10//if value is there then assig it else affign null
+
+
+// console.log(val1);
+
+
+// ternery operator
+// condition ? true : false
+const iceTeaPrice = 100
+iceTeaPrice >=80 ? console.log("true"):console.log("False")
