0a77d19c02cb6bdeb8078e2b632bac7c4c004442
Gaurav <2k22.csai.32026@gmail.com>
2026-02-28 17:40:03 +0530

    object1 done

---
+++ .devcontainer/01_basics/objects1.js
@@ -0,0 +1,36 @@
+//singleton
+
+//object literals
+// Object.create//constructer method
+
+const mySym = Symbol("Key1")//declaring a symbol
+
+
+const user={
+    name:"Gaurav",
+    "full name":"Gaurav Jaiswal",
+    [mySym]:"myKey1",//symbol is add in the object
+    roll:72,
+    branch:"CS-AI",
+    email:"abc@gmail.com",
+    isLoggedIn:false,
+    lastLoggedIn:["monday","tuesday"]
+
+}
+
+//2 methods to access values
+console.log(user.name);//method 1
+console.log(user["full name"]);//method 2
+console.log(user[mySym]);
+
+// Object.freeze(user)//freeze all the valuse of the object
+
+user.name="Saurav"
+
+// console.log(user);//value didn't change because of freeze method
+
+user.greeting=function () {
+    console.log(`Hello JS users,${this.name}`);
+    
+}
+console.log(user.greeting());
