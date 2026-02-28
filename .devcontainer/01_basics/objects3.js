7e8de88c4ba55e7d9c29b77c68a940257e58e145
Gaurav <2k22.csai.32026@gmail.com>
2026-02-28 18:42:54 +0530

    commit

---
+++ .devcontainer/01_basics/objects3.js
@@ -0,0 +1,12 @@
+const course={
+    cname:"JS in Hindi",
+    cprice:"999",
+    cteacher:"hitesh"
+
+}
+
+const {cteacher:instructor}=course//destructuring
+// console.log(cteacher);
+console.log(instructor);
+
+
