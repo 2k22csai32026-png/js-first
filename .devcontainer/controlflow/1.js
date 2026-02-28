cac2f7ef23d8c2b3753fca4d5a4f0382e1606b96
Gaurav <2k22.csai.32026@gmail.com>
2026-03-01 02:05:34 +0530

    changes

---
+++ .devcontainer/controlflow/1.js
@@ -0,0 +1,11 @@
+if(2=="2"){
+    console.log("true");
+    
+}
+
+const balance=1000
+if(balance>500) console.log("true");
+
+//****************Falsy Values*********************//
+
+// false, 0, -0, BigInt 0n, "", null, undefined, NaN 
