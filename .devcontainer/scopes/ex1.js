ddc23ccec538cbce815ccc51edd3a3fb58333940
Gaurav <2k22.csai.32026@gmail.com>
2026-02-28 22:48:20 +0530

    change

---
+++ .devcontainer/scopes/ex1.js
@@ -0,0 +1,13 @@
+
+if(true){
+
+    let a=10
+    const b=20
+    var c=30
+
+}
+
+// console.log(a); not run because the scope is inside the {}
+// console.log(b); not run because the scope is inside the {}
+console.log(c);//run because var has no scope and creates problem
+
