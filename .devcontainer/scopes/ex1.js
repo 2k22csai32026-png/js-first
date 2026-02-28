cd669f2a9249814067504831a5a60c598f5e9295
Gaurav <2k22.csai.32026@gmail.com>
2026-02-28 23:04:06 +0530

    change

--- .devcontainer/scopes/ex1.js
+++ .devcontainer/scopes/ex1.js
@@ -9,5 +9,21 @@ if(true){
 
 // console.log(a); not run because the scope is inside the {}
 // console.log(b); not run because the scope is inside the {}
-console.log(c);//run because var has no scope and creates problem
+// console.log(c);//run because var has no scope and creates problem
 
+function one(){
+    const username="Gaurav"
+
+    function two(){
+        const website="Youtube"
+        console.log(username);
+        
+        
+    }
+    // console.log(website);//throws an error because it is out of the scope of two()
+
+    two()
+}
+one()
+// one() cannot access the variables pf the two()
+// but two() can get the access of one()
