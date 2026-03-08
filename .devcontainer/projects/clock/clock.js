6bea4cb69b331e1846316aed5f1c8c63882a0d69
Gaurav <2k22.csai.32026@gmail.com>
2026-03-08 14:23:34 +0530

    new technique

--- .devcontainer/projects/clock/clock.js
+++ .devcontainer/projects/clock/clock.js
@@ -1,9 +1,13 @@
 const clock=document.querySelector('#clock')
+
+// let hours = now.getHours();
+// let minutes = now.getMinutes();
+// let seconds = now.getSeconds();
+
+// const time=hours+" : "+minutes+" : "+seconds
+
+// clock.innerHTML=time
+setInterval(function(){
 const now=new Date()
-let hours = now.getHours();
-let minutes = now.getMinutes();
-let seconds = now.getSeconds();
-
-const time=hours+" : "+minutes+" : "+seconds
-
-clock.innerHTML=time
+clock.innerHTML=now.toLocaleTimeString();
+},1000);
