ab865aa1a5e69393fb23ea6cb3f630ff2a2efd2b
Gaurav <2k22.csai.32026@gmail.com>
2026-03-10 01:48:49 +0530

    unlimited color

---
+++ .devcontainer/projects/unlimitedColor/unlimitedColor.js
@@ -0,0 +1,25 @@
+const randomColor = function () {
+  const hex = '0123456789ABCDEF'
+  let color = '#'
+
+  for (let i = 0; i < 6; i++) {
+    color += hex[Math.floor(Math.random() * 16)]
+  }
+
+  return color
+}
+
+let validId
+
+document.querySelector('#start').addEventListener('click', function () {
+
+  validId = setInterval(function () {
+    document.body.style.backgroundColor = randomColor()
+  }, 1000)
+
+})
+
+document.querySelector('#stop').addEventListener('click', function () {
+  clearInterval(validId)
+  validId=null;
+})
